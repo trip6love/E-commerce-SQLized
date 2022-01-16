6eac33bc5f85b705d7576eccf0e7378dda775353
trip6love <lanebyers13@gmail.com>
2022-01-16 14:26:17 -0500

    defined columns in CategoryJS

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,20 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define columns //
+    id: {
+      type: DataTypes, INTEGER,
+      allowNull : false, 
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // catagory name col //
+    catagory_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+
+    }
+
   },
   {
     sequelize,
