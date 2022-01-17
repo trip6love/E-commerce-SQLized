9767eb98f016004dfa67f80e88176c685136949e
trip6love <lanebyers13@gmail.com>
2022-01-16 19:06:14 -0500

    started defining producttag and did a quick fix to categoryJS

--- models/Category.js
+++ models/Category.js
@@ -8,7 +8,7 @@ Category.init(
   {
     // define columns //
     id: {
-      type: DataTypes, INTEGER,
+      type: DataTypes.INTEGER,
       allowNull : false, 
       primaryKey: true,
       autoIncrement: true
